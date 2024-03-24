51d7b5895113d504db872defe651d9e63d1a8540
Vladislav Kalambet <vlad.kalambet@yandex.ru>
2024-03-24 12:39:45 +0000

    blog

--- push_db.py
+++ push_db.py
@@ -3,8 +3,6 @@ from sqlalchemy import create_engine, Column, Integer, String, Text
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import declarative_base
 
-from flask import Flask, render_template, request
-
 # Настройка базы данных
 engine = create_engine('sqlite:///blog.db')
 Base = declarative_base()
@@ -24,21 +22,7 @@ Session = sessionmaker(bind=engine)
 session = Session()
 
 
-
-app = Flask(__name__)
-
-@app.route("/")
-def index():
-    posts = session.query(Post).all()
-    return render_template("index.html", posts=posts)
-
-
-@app.route('/add', methods = ['GET', 'POST'])
-def add():
-    if request.method == 'POST':
-       title = request.form['title']
-       content = request.form['content']
-       post = Post(title = title, content = content)
-       session.add(post)
-       session.commit()
-    return render_template('add.html')
+for i in range(1, 5):
+    my_first_post = Post(title = f'Привет Мир {i}!', content = 'Содержимое записи привет мир!')
+    session.add(my_first_post)
+    session.commit()
